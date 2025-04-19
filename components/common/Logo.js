d4972686002d99cdf4d6384c84a71a3101ac1abb
Aman <amanpanchal8397@gmail.com>
2025-04-19 15:04:34 +0530

    done

--- components/common/Logo.js
+++ components/common/Logo.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 
 const Logo = ({variant = 'one', fillTextColor = '#00004B'}) => {
     return (
-      <Image src={"/images/others/logo.jpg"} width="200" height="100" />
+      <Image
+        src={"/images/others/Nextdev Lab icon mark.svg"}
+        width="200"
+        height="100"
+      />
     );
 }
 
