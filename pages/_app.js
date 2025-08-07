c9970f51926b6826dbb28b4428fe0b6c23db3223
abhishekp8003 <pledgeing3@gmail.com>
2025-08-07 09:06:51 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ const structuredData = [
       "https://firebasestorage.googleapis.com/v0/b/imageuploader-7809e.appspot.com/o/Monogram%20colour.svg?alt=media&token=3446b4a6-29b5-457e-908c-d9371470e622",
     contactPoint: {
       "@type": "ContactPoint",
-      telephone: "+91 97678 26126",
+      telephone: "+91 9511838696 ",
       contactType: "customer service",
       areaServed: "IN",
       availableLanguage: ["English", "Hindi"],
