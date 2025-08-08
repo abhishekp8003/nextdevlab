0f01f5792d67f0971eac71ef94257d88fe3019aa
abhishekp8003 <pledgeing3@gmail.com>
2025-08-08 17:40:49 +0530

    Update send-email.js

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
     const mailOptions = {
       from: "hello2appmechanic@gmail.com",
-      to: "nextdevlabs@gmail.com",
+      to: "nextdevlabs7@gmail.com",
       // to : "amanpanchal144@gmail.com",
       subject: "Email from website",
       text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage: ${message}`,
